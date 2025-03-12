3f99681c61f450b9de5109d11169d713e1ffe628
Harunene <taskinrits@gmail.com>
2025-03-12 21:38:17 +0900

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from "@vercel/analytics/react"
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,6 +15,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <main>{children}</main>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   )
